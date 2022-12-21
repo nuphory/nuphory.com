db0be0077088d6587bc4dcc4a1ce4cd9f39595b5
mia-cx <mia@mia.cx>
2022-12-21 14:28:56 +0100

    testing platform env

---
+++ src/routes/api/products/server.ts
@@ -0,0 +1,40 @@
+import { error } from '@sveltejs/kit';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+/** @type {import('./$types').RequestHandler} */
+export async function get() {
+    return {
+      status: 200,
+      headers: {
+        'access-control-allow-origin': '*'
+      },
+      body: {
+        number: Math.random()
+      }
+    };
+  }
+  
+
+// async function fetchProducts() {
+
+//     // prinftul api endpoint
+//     const endpoint = "https://api.printful.com/products";
+
+//     const headers = {
+//         "Authorization": `Basic ${PRINTFUL_API_KEY}`,
+//         "Content-Type": "application/json",
+//     }
+
+//     try {
+//         const response = await fetch(endpoint, {
+//             headers,
+//         });
+
+//         const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// }
