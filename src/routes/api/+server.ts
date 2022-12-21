43b704316bc4baa232842dca54d8d034f9738366
mia-cx <mia@mia.cx>
2022-12-21 14:58:42 +0100

    security stuff

--- src/routes/api/+server.ts
+++
@@ -1,7 +0,0 @@
-import { error } from '@sveltejs/kit';
-import { PRINTFUL_API_TOKEN } from '$env/static/private';
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET() {
-    return new Response(PRINTFUL_API_TOKEN);
-  }
