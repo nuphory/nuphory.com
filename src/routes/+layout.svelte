b31864a7624186012e77c2c5209999ae0e57f491
mia-cx <mia@mia.cx>
2022-12-21 18:10:09 +0100

    seo template from nuphory.com + remove example app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,52 @@
-<script>
-	import Header from './Header.svelte';
+<script lang="ts">
+	import { _siteName, _tagLine } from './+layout';
+	import { browser } from '$app/environment';
+
 	import './styles.css';
+	/**
+	 * website designed by patch: https://twitter.com/patchstep
+	 */
+	console.log(
+		`%c${_siteName}%c\n\n\n${_siteName}.com was made by %cPatch\n%chttps://twitter.com/patchstep`,
+		"font-family: 'Yeysk'; font-size: 6em; font-weight: bold; text-stroke: 2px #272d3b; color: #fffaf7;",
+		'font-size: 2em; color: #fffaf7;',
+		'font-size: 2em; color: #fffaf7; font-weight: bold;',
+		'font-size: 1em; color: #fffaf7; font-weight: bold;'
+	);
 </script>
 
+<svelte:head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon/favicon-194x194.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="192x192"
+    href="/assets/favicon/android-chrome-192x192.png"
+  />
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+  <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+  <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#272d3b" />
+  <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+  <meta name="apple-mobile-web-app-title" content="nuphory" />
+  <meta name="application-name" content="nuphory" />
+  <meta name="msapplication-TileColor" content="#272d3b" />
+  <meta name="msapplication-TileImage" content="/assets/favicon/mstile-144x144.png" />
+  <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml" />
+  <meta name="theme-color" content="#fffaf7" />
+
+  <meta property="og:site_name" content={_siteName} />
+</svelte:head>
+
 <div class="app">
-	<Header />
+	<!-- <header /> -->
 
 	<main>
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<footer />
 </div>
 
 <style>
--- src/routes/api/random-number/+server.ts
+++
@@ -1,8 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET() {
-    return new Response(Math.random());
-  }
-  
-
