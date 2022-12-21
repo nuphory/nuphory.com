5413727f8b760d62b0861528249fbd6d62277d2f
mia-cx <mia@mia.cx>
2022-12-21 23:28:50 +0100

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,34 @@
 	import { _siteName, _tagLine } from './+layout';
 	import { browser } from '$app/environment';
 
+	import Footer from '$lib/components/layout/Footer.svelte';
+
 	import './styles.css';
+
+	(async () => {
+		if (!browser) return;
+
+		let collapsibles = document.querySelectorAll('.collapsible');
+		collapsibles.forEach((collapsible) => {
+			collapsible.addEventListener('click', () => {
+				collapsible.classList.toggle('active');
+				let content = collapsible.nextElementSibling as HTMLElement;
+				if (!content) return;
+
+				// toggle content maxheight
+				if (content.style.maxHeight) {
+					content.style.removeProperty('max-height');
+				} else {
+					content.style.maxHeight = content.scrollHeight + 'px';
+				}
+			});
+		});
+
+		const FadeElement = (await import('$lib/fade-in')).default;
+		customElements.define('fade-in', FadeElement);
+		FadeElement.fadeInAll(100);
+	})();
+
 	/**
 	 * website designed by patch: https://twitter.com/patchstep
 	 */
@@ -16,27 +43,27 @@
 </script>
 
 <svelte:head>
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon/favicon-194x194.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="192x192"
-    href="/assets/favicon/android-chrome-192x192.png"
-  />
-  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
-  <link rel="manifest" href="/assets/favicon/site.webmanifest" />
-  <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#272d3b" />
-  <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
-  <meta name="apple-mobile-web-app-title" content="nuphory" />
-  <meta name="application-name" content="nuphory" />
-  <meta name="msapplication-TileColor" content="#272d3b" />
-  <meta name="msapplication-TileImage" content="/assets/favicon/mstile-144x144.png" />
-  <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml" />
-  <meta name="theme-color" content="#fffaf7" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon/favicon-194x194.png" />
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="192x192"
+		href="/assets/favicon/android-chrome-192x192.png"
+	/>
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+	<link rel="manifest" href="/assets/favicon/site.webmanifest" />
+	<link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#272d3b" />
+	<link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+	<meta name="apple-mobile-web-app-title" content="nuphory" />
+	<meta name="application-name" content="nuphory" />
+	<meta name="msapplication-TileColor" content="#272d3b" />
+	<meta name="msapplication-TileImage" content="/assets/favicon/mstile-144x144.png" />
+	<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml" />
+	<meta name="theme-color" content="#fffaf7" />
 
-  <meta property="og:site_name" content={_siteName} />
+	<meta property="og:site_name" content={_siteName} />
 </svelte:head>
 
 <div class="app">
@@ -46,10 +73,28 @@
 		<slot />
 	</main>
 
-	<footer />
+	<fade-in id="book-me" distance="10px" duration="500ms" order="15">
+		<a href="#contact" id="book-me-link">
+			<h4>book me</h4>
+			<h4>v</h4>
+		</a>
+	</fade-in>
+
+	<footer>
+		<Footer />
+	</footer>
 </div>
 
 <style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		background-color: var(--foreground-color);
+		color: var(--background-color);
+		padding: 16px 0px;
+		width: 100%;
+	}
 	.app {
 		display: flex;
 		flex-direction: column;
@@ -77,6 +122,20 @@
 
 	footer a {
 		font-weight: bold;
+	}
+
+	#book-me {
+		text-align: center;
+		padding: 16px 0px;
+	}
+	#book-me-link:hover {
+		text-decoration: none;
+	}
+	#book-me h4 {
+		margin: 0px;
+	}
+	#book-me h4:nth-child(2) {
+		margin-bottom: 16px;
 	}
 
 	@media (min-width: 480px) {
