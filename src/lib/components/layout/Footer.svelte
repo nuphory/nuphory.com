71fc189f2e471d445c65f0862c7a474b7e14a694
mia-cx <mia@mia.cx>
2022-12-23 08:48:54 +0100

    tailwind

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,78 +1,80 @@
 <script>
-  import Impressum from '$lib/components/layout/Impressum.svelte';
+	import Impressum from '$lib/components/layout/Impressum.svelte';
 </script>
 
 <div id="title"><h4>nuphory</h4></div>
 
 <div id="content">
-  <section id="impressum" class="footer-area">
-    <Impressum />
-  </section>
+	<section id="impressum" class="footer-area">
+		<Impressum />
+	</section>
 
-  <section id="contact" class="footer-area">
-    <h4>contact</h4>
-    <ul>
-      <!-- link opens in new tab-->
-      <li>e-mail: <a href="mailto:nuphory@gmail.com" target="_blank">nuphory@gmail.com</a></li>
-    </ul>
-  </section>
+	<section id="contact" class="footer-area">
+		<h4 class="grow">contact</h4>
+		<ul>
+			<!-- link opens in new tab-->
+			<li>e-mail: <a href="mailto:nuphory@gmail.com" target="_blank">nuphory@gmail.com</a></li>
+		</ul>
+	</section>
 </div>
 
 <div id="copyright">© 2022 nuphory</div>
 
 <style>
-  section {
-    margin: 16px;
-  }
 
-  ul {
-    list-style-type: none;
-    padding: 0px;
-  }
+	.footer-area {
+		@apply w-64;
+		@apply text-left;
+	}
 
-  a {
-    color: var(--background-color);
-  }
+	ul {
+		list-style-type: none;
+		padding: 0px;
+	}
 
-  a:hover {
-    color: var(--background-color);
-  }
+	a {
+		color: var(--color-text);
+	}
 
-  .footer-area {
-    width: 250px;
-    /* display: inline-block; */
-    text-align: left;
-  }
+	a:hover {
+		color: var(--color-text);
+	}
 
-  #title {
-    text-align: center;
-    margin: 16px;
-  }
+	.footer-area {
+		width: 250px;
+		/* display: inline-block; */
+		text-align: left;
+	}
 
-  #content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+	#title {
+		text-align: center;
+		margin: 16px;
+	}
 
-  #contact {
-    text-align: left;
-    margin-bottom: 16px;
-  }
+	#content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 
-  #copyright {
-    border-top: 1px solid var(--background-color);
-    text-align: center;
-    padding-top: 16px;
-  }
+	#contact {
+		text-align: left;
+		margin-bottom: 16px;
+	}
 
-  @media only screen and (min-width: 768px) {
-    #content {
-      height: 300px;
-      flex-direction: row;
-      justify-content: center;
-      align-items: flex-start;
-    }
-  }
+	#copyright {
+		border-top: 1px solid var(--color-text);
+		text-align: center;
+		padding-top: 16px;
+	}
+
+	@media only screen and (min-width: 768px) {
+		#content {
+			height: 300px;
+			flex-direction: row;
+			justify-content: center;
+			align-items: flex-start;
+		}
+	}
 </style>
