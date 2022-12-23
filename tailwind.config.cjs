d472b1113f39cad45384e17bb533a2b45b241407
mia-cx <mia@mia.cx>
2022-12-23 08:46:55 +0100

    tailwind + scss

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('@tailwindcss/typography'),require('@tailwindcss/line-clamp'),require('@tailwindcss/forms')]
+};
