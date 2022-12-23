4f9db4f40eff232713de247b2848eed2b725a9c6
mia-cx <mia@mia.cx>
2022-12-23 08:50:38 +0100

    header for cart

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+        import CartButton from "../store/cart/CartButton.svelte";
+</script>
+
+<section class="flex flex-row justify-between items-center max-w-5xl w-full">
+        <div />
+        <div class="title"></div>
+        <CartButton />
+</section>
