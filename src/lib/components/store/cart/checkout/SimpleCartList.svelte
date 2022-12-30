5b813d75c92db74b88bd1c0714f0a9a27cd1f15b
mia-cx <mia@mia.cx>
2022-12-30 03:11:58 +0100

    fix simplecartlist & item with order refactor

--- src/lib/components/store/cart/checkout/SimpleCartList.svelte
+++ src/lib/components/store/cart/checkout/SimpleCartList.svelte
@@ -1,25 +1,21 @@
 <script lang="ts">
-        import { browser } from '$app/environment';
-        import type { SyncVariant } from '$lib/api/product';
-        import { cart as cartStore, type CartItem, type CartMap } from '$lib/api/stores/cart';
-        import { order } from '$lib/api/stores/order';
-        import recipientStore, { type Recipient } from '$lib/api/stores/recipient';
-
+        import currentOrder from '$lib/api/stores/order';
+        import type { Item } from '$lib/types/product';
         import SimpleCartListItem from './SimpleCartListItem.svelte';
 
-        if (browser) {
-                cartStore.subscribe((cart) => {
-                        cartItems = Array.from(cart);
-                });
-        }
+        let items: Item[] = $currentOrder.items;
+
+        currentOrder.subscribe((order) => {
+                items = order.items;
+        });
 </script>
 
 <ul
         id="cart"
         class=" m-4 mb-0 w-80 flex flex-col justify-center items-center rounded-[2em] border-[3px]"
 >
-        {#each cartItems as item}
-                <SimpleCartListItem item={item[1]} />
+        {#each items as item}
+                <SimpleCartListItem item={item} />
         {/each}
         <li
                 class="w-full  flex flex-row basis-1 justify-between items-start px-4 m-4 gap-4 xs:items-start last:border-[0px] border-b clr-border"
@@ -31,8 +27,8 @@
                                 <p class="">Subtotal</p>
                                 <!-- <p class="">—</p> -->
                                 <p class="text-right sm:w-auto">
-                                        {$order.retail_costs.subtotal}
-                                        {$order.retail_costs.currency}
+                                        {$currentOrder.retail_costs.subtotal}
+                                        {$currentOrder.retail_costs.currency}
                                 </p>
                         </div>
                         <div
@@ -41,8 +37,8 @@
                                 <p class="">VAT (19%)</p>
                                 <!-- <p class="">—</p> -->
                                 <p class="text-right sm:w-auto">
-                                        {$order.retail_costs.tax}
-                                        {$order.retail_costs.currency}
+                                        {$currentOrder.retail_costs.tax}
+                                        {$currentOrder.retail_costs.currency}
                                 </p>
                         </div>
                         <div
@@ -51,8 +47,8 @@
                                 <p class="">Shipping</p>
                                 <!-- <p class="">—</p> -->
                                 <p class="text-right sm:w-auto">
-                                        {$order.retail_costs.shipping}
-                                        {$order.retail_costs.currency}
+                                        {$currentOrder.retail_costs.shipping}
+                                        {$currentOrder.retail_costs.currency}
                                 </p>
                         </div>
                         <div class="flex flex-row flex-1 grow basis-1 justify-between items-start">
@@ -60,11 +56,11 @@
                                 <!-- <p class="">—</p> -->
                                 <p class="text-right sm:w-auto">
                                         {(
-                                                parseFloat($order.retail_costs.subtotal) +
-                                                parseFloat($order.retail_costs.shipping) +
-                                                parseFloat($order.retail_costs.tax)
+                                                parseFloat($currentOrder.retail_costs.subtotal) +
+                                                parseFloat($currentOrder.retail_costs.shipping) +
+                                                parseFloat($currentOrder.retail_costs.tax)
                                         ).toFixed(2)}
-                                        {$order.retail_costs.currency}
+                                        {$currentOrder.retail_costs.currency}
                                 </p>
                         </div>
                 </div>
--- src/lib/components/store/cart/checkout/SimpleCartListItem.svelte
+++ src/lib/components/store/cart/checkout/SimpleCartListItem.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-        import type { CartItem } from '$lib/api/stores/cart';
+        import currentOrder from "$lib/api/stores/order";
+        import type { Item } from "$lib/types/product";
 
-        export let item: CartItem;
+        export let item: Item;
 </script>
 
 <li
         class="flex flex-row basis-1 justify-between items-start m-4 gap-4 xs:items-start last:border-[0px] border-b clr-border"
 >
         <img
-                src={item.variant.files[1].preview_url}
-                alt={item.variant.name}
+                src={item.files[1].preview_url}
+                alt={item.name}
                 class="aspect-square w-full max-w-[4em] rounded-2xl border-[1px]"
         />
 
         <div class="flex flex-1 flex-col justify-between gap-4">
-                <p class="border-b">{item.variant.name}</p>
+                <p class="border-b">{item.name}</p>
                 <div class="flex-1 flex flex-col justify-between items-start overflow-auto">
                         <div class="w-full flex flex-col overflow-auto">
                                 <div class="flex flex-col justify-between items-start">
                                         <div class="flex flex-row justify-between w-full">
                                                 <p class="text-left w-full">
-                                                        {item.variant.id}
+                                                        {item.id}
                                                 </p>
                                                 <p class="text-right w-full">
                                                         {item.quantity}x
@@ -28,8 +29,8 @@
                                         </div>
                                         <!-- <p class="">—</p> -->
                                         <p class="text-right w-full">
-                                                {item.variant.retail_price}
-                                                {item.variant.currency}
+                                                {item.retail_price}
+                                                {item.currency}
                                         </p>
                                 </div>
                         </div>
@@ -37,9 +38,9 @@
                                 <p>Subtotal</p>
                                 <p class="text-right w-full">
                                         {(
-                                                parseInt(item.variant.retail_price) * item.quantity
+                                                parseInt(item.retail_price) * item.quantity
                                         ).toFixed(2)}
-                                        {item.variant.currency}
+                                        {item.currency}
                                 </p>
                         </div>
                 </div>
