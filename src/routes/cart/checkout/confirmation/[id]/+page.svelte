d28a0981ce0fa3a0de349f2ebeed37560f0ed377
mia-cx <mia@mia.cx>
2022-12-31 21:39:35 +0100

    checkout page edge case

--- src/routes/cart/checkout/confirmation/[id]/+page.svelte
+++ src/routes/cart/checkout/confirmation/[id]/+page.svelte
@@ -45,26 +45,49 @@
 </svelte:head>
 
 <div id="top" class="relative flex flex-1 flex-col justify-center items-center min-h-screen py-8">
-        <section id="page-title" class="pointer-events-none">
-                <h1 class="tracking-[0.125em]">confirmed</h1>
-        </section>
-        <div class="content flex flex-col flex-1 justify-center items-start">
-                <p class="w-full text-center">{`ORDER ID: ${order.external_id}`}</p>
-                <section
-                        id="cart-list"
-                        class="w-80 relative flex flex-col justify-center items-center lg:order-2"
-                >
-                        <SimpleCartList items={order.items} />
-                        <section id="back-to-home" class="mb-0 flex justify-center items-center ">
-                                <a
-                                        id="checkout-button"
-                                        class="w-80 badge py-1 px-4 clr-bg clr-text clr-inverse !rounded-full font-mono"
-                                        href="/"><b>back to home</b></a
-                                >
-                        </section>
+        {#if order}
+                <!-- content here -->
+                <section id="page-title" class="pointer-events-none">
+                        <h1 class="tracking-[0.125em]">confirmed</h1>
                 </section>
-                <!-- <section id="checkout-form" class="max-w-2xl lg:order-1">
-                        <CheckoutForm />
-                </section> -->
-        </div>
+                <div class="content flex flex-col flex-1 justify-center items-start">
+                        <p class="w-full text-center">{`ORDER ID: ${order.external_id}`}</p>
+                        <section
+                                id="cart-list"
+                                class="w-80 relative flex flex-col justify-center items-center lg:order-2"
+                        >
+                                <SimpleCartList items={order.items} />
+                                <section id="back-to-home" class="mb-0 flex justify-center items-center ">
+                                        <a
+                                                id="checkout-button"
+                                                class="w-80 badge py-1 px-4 clr-bg clr-text clr-inverse !rounded-full font-mono"
+                                                href="/"><b>back to home</b></a
+                                        >
+                                </section>
+                        </section>
+                        <!-- <section id="checkout-form" class="max-w-2xl lg:order-1">
+                                <CheckoutForm />
+                        </section> -->
+                </div>
+        {:else}
+                <section id="page-title" class="pointer-events-none">
+                        <h1 class="tracking-[0.125em]">order not found</h1>
+                </section>
+                <div class="content flex flex-col flex-1 justify-center items-start">
+                        <p class="w-full text-center">The order with id {data.order_id} was either not confirmed, or doesn't exist.</p>
+                        <p class="w-full text-center">If you believe this to be an error, please contact <a href="mailto:nuphory@gmail.com">nuphory@gmail.com</a></a></p>
+                        <section
+                                id="cart-list"
+                                class="w-80 relative flex flex-col justify-center items-center lg:order-2"
+                        >
+                                <section id="back-to-home" class="mb-0 flex justify-center items-center ">
+                                        <a
+                                                id="checkout-button"
+                                                class="w-80 badge py-1 px-4 clr-bg clr-text clr-inverse !rounded-full font-mono"
+                                                href="/"><b>home</b></a
+                                        >
+                                </section>
+                        </section>
+                </div>
+        {/if}
 </div>
