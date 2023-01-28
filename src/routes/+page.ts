e56b674612997d493214295cac923afe9f1b63b4
mia-cx <mia@mia.cx>
2023-01-28 04:47:02 +0100

    wretch all products

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,20 +1,30 @@
-import type { SyncProduct } from '$lib/api/product';
+import { api } from '$lib/api/internalApi';
+
+import type { Product, SyncProduct } from '$lib/types/product';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
-        const response = await fetch('/api/products');
-        const data = await response.json();
+export async function load() {
+        try {
+                const res = api.url('/products').get().res();
 
-        const products = await Promise.all(
-                data.result.map(async (product: SyncProduct) => {
-                        const response = await fetch(`/api/products/${product.id}`);
-                        const data = await response.json();
+                const json = await (await res).json();
 
-                        return data.result;
-                })
-        );
+                // console.debug('products', json);
 
-        return { products };
+                const sync_products = json.result as SyncProduct[];
+
+                const products = sync_products.map(async (sync_product: SyncProduct) => {
+                        const res = api.url(`/products/${sync_product.id}`).get().res();
+
+                        const json = await (await res).json();
+
+                        // console.debug('product', json);
+
+                        return json.result as Product;
+                });
+
+                return { products: await Promise.all(products) };
+        } catch (error) {
+                console.error(error);
+        }
 }
-
-// export const ssr = false;
