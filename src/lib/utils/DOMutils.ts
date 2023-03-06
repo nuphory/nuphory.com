2f4be5e3173489cf3255e9954c7063df22003e0c
mia-cx <mia@mia.cx>
2023-03-06 06:53:05 +0100

    viewport checker

---
+++ src/lib/utils/DOMutils.ts
@@ -0,0 +1,11 @@
+export function isElementInViewport(el: HTMLElement) {
+        if (!el) return false;
+
+        const rect = el.getBoundingClientRect();
+        return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+}
