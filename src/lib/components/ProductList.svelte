c5d7899af92c3b4f2931c797c45ce8a7e860b7ea
mia-cx <mia@mia.cx>
2023-03-06 09:10:42 +0100

    implement skeleton loading

--- src/lib/components/ProductList.svelte
+++ src/lib/components/ProductList.svelte
@@ -1,15 +1,51 @@
 <script lang="ts">
-        import type { Product } from '$lib/types/product';
+        import type { Product, SyncProduct } from '$lib/types/product';
+        import { api } from '../api/internalApi';
         import ProductListItem from './ProductListItem.svelte';
+
+        async function fetchProducts() {
+                try {
+                        const res = api.url('/products').get().res();
+
+                        const json = await (await res).json();
+
+                        // console.debug('products', json);
+
+                        const sync_products = json.result as SyncProduct[];
+
+                        const products = sync_products.map(async (sync_product: SyncProduct) => {
+                                const res = api.url(`/products/${sync_product.id}`).get().res();
+
+                                const json = await (await res).json();
+
+                                // console.debug('product', json);
+
+                                return json.result as Product;
+                        });
+
+                        return await Promise.all(products);
+                } catch (error) {
+                        return [];
+                        console.error(error);
+                }
+        }
 
         export let products: Product[] = [];
 </script>
 
 <ul id="products" class="flex flex-col justify-center items-center md:flex-row">
-        {#each products as product}
-                <!-- TODO filter ignored products in the server-side GET request -->
-                {#if !product.sync_product.is_ignored}
+        {#await fetchProducts()}
+                {#each Array(2) as product, i}
+                        <!-- <li>{i}</li> -->
                         <ProductListItem {product} />
-                {/if}
-        {/each}
+                {/each}
+        {:then products}
+                <!-- promise was fulfilled -->
+                {#each products as product}
+                        <!-- TODO filter ignored products in the server-side GET request -->
+                        {#if !product.sync_product.is_ignored}
+                                <ProductListItem {product} />
+                        {/if}
+                {/each}
+        {/await}
 </ul>
--- src/lib/components/ProductListItem.svelte
+++ src/lib/components/ProductListItem.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+        import { fade } from 'svelte/transition';
         import type { Product, SyncVariant } from '$lib/types/product';
         import currentOrder from '$lib/api/stores/order';
+        import CartPlus from './icons/CartPlus.svelte';
 
         export let product: Product;
 
@@ -30,56 +32,128 @@
         }
 </script>
 
-<li
-        class="flex flex-col justify-center items-center clr-regular m-4 p-4 rounded-[2em] border-[3px]"
->
-        <!-- <a href="/shop/{product.id}"> -->
-        <div class="showcase relative flex flex-col justify-center items-center">
-                <img
-                        src={product.sync_product.thumbnail_url}
-                        alt={product.sync_product.name}
-                        class="aspect-square w-full max-w-xs rounded-2xl border-[1px]"
-                />
-                <span
-                        class="price absolute right-2 bottom-0 py-1 px-3 translate-y-1/3 badge border-[3px] clr-bg clr-inverse font-mono"
-                >
-                        {selectedVariant
-                                ? selectedVariant.retail_price
-                                : product.sync_variants[0].retail_price}
-                        {selectedVariant
-                                ? selectedVariant.currency
-                                : product.sync_variants[0].currency}
-                </span>
-        </div>
-        <h4>{product.sync_product.name}</h4>
+<li class="w-xs m-4 p-4 rounded-[2em] outline outline-[3px]">
+        <!-- product listing -->
 
-        <div class="product-selection flex flex-row justify-center items-center gap-4">
+        <!-- product showcase -->
+        <div class="relative">
+                <!-- product image -->
+                {#if product}
+                        <img
+                                in:fade={{ duration: 500 }}
+                                src={product.sync_product?.thumbnail_url}
+                                alt={product.sync_product?.name}
+                                class="
+                                        relative 
+                                        aspect-square 
+                                        w-80 max-w-xs 
+                                        rounded-2xl outline outline-[1px] 
+                                        bg-white
+                                "
+                        />
+                {:else}
+                        <div
+                                in:fade={{ duration: 500 }}
+                                class="
+                                                relative 
+                                                aspect-square 
+                                                w-80 max-w-xs 
+                                                rounded-2xl outline outline-[1px] 
+                                                bg-white
+                                        "
+                        >
+                                <h4
+                                        class="absolute bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2 text-neutral-200"
+                                >
+                                        Loading...
+                                </h4>
+                        </div>
+                {/if}
+                <!-- product price -->
+                {#if product}
+                        <span
+                                in:fade={{ duration: 500 }}
+                                class="
+                                        absolute 
+                                        right-2 bottom-0 
+                                        translate-y-1/3 
+                                        py-1 px-3 
+                                        font-mono
+
+                                        outline outline-[3px] outline-[var(--text-invert)]
+
+                                        clr-bg-invert clr-text-invert
+                                        badge
+                                "
+                        >
+                                {selectedVariant
+                                        ? selectedVariant.retail_price
+                                        : product.sync_variants[0].retail_price}
+                                {selectedVariant
+                                        ? selectedVariant.currency
+                                        : product.sync_variants[0].currency}
+                        </span>
+                {:else}
+                        <span
+                                in:fade={{ duration: 500 }}
+                                class="
+                                        absolute 
+                                        right-2 bottom-0 
+                                        translate-y-1/3 
+                                        py-1 px-3 
+                                        font-mono
+
+                                        outline outline-[3px] outline-[var(--text-invert)]
+
+                                        clr-bg-invert clr-text-invert
+                                        badge
+                                "
+                        >
+                                -.-- EUR
+                        </span>
+                {/if}
+        </div>
+        <!-- product name -->
+        {#if product}
+                <h4 in:fade={{ duration: 500 }} class="text-center">
+                        {product.sync_product.name}
+                </h4>
+        {:else}
+                <h4 in:fade={{ duration: 500 }} class="text-center">Loading...</h4>
+        {/if}
+        <!-- size selection -->
+
+        <div class="flex flex-row justify-between items-center gap-4">
                 <select
-                        class="badge border-[1px] clr-border clr-bg"
+                        class="grow badge outline outline-[1px] clr-bg-invert clr-text-invert"
                         name="variant"
                         on:change={selectVariant}
                 >
-                        <option value="0" disabled selected>Select your size</option>
-                        {#each product.sync_variants as variant}
-                                <option value={variant.id}>{variant.name}</option>
-                        {/each}
+                        {#if product}
+                                <option in:fade={{ duration: 500 }} value="0" disabled selected
+                                        >Select your size</option
+                                >
+                                {#each product.sync_variants as variant}
+                                        <option in:fade={{ duration: 500 }} value={variant.id}
+                                                >{variant.name}</option
+                                        >
+                                {/each}
+                        {:else}
+                                <option in:fade={{ duration: 500 }} value="0" disabled selected
+                                        >Loading sizes...</option
+                                >
+                        {/if}
                 </select>
-                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
                 <button
                         class="	cart-button badge aspect-square cursor-pointer  
-				clr-bg clr-inverse 
+				clr-bg-invert clr-text-invert 
 				hover:scale-105 active:scale-95
 				disabled"
                         on:click={addItem}
                         on:keydown={addItem}
                         tabindex="0"
                 >
-                        <img
-                                width="18px"
-                                height="18px"
-                                alt="add to cart"
-                                src="/assets/icons/cart-plus-solid.svg"
-                        />
+                        <CartPlus height="18px" />
                 </button>
         </div>
 </li>
