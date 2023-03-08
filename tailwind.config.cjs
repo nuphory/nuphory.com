225b8c471e286062c28452eb4534e423566c4195
mia-cx <mia@mia.cx>
2023-03-08 03:58:42 +0100

    tailwind theming :)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
         content: ['./src/**/*.{html,js,svelte,ts}'],
         theme: {
-                extend: {}
+                extend: {
+                        ringWidth: {
+                                3: '3px'
+                        },
+                        transitionProperty: {
+                                colors: 'color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow',
+                                layout: 'inset,transform,height,min-height,max-height,width,min-width,max-width,margin,padding,outline-color,opacity,font-size',
+                                quick: 'box-shadow,color,background-color,border-color,text-decoration-color,fill,stroke,inset,transform,height,min-height,max-height,width,min-width,max-width,margin,padding,outline-color,opacity,font-size'
+                        }
+                },
+                colors: {
+                        primary: 'var(--clr-primary)',
+                        secondary: 'var(--clr-secondary)'
+                }
         },
         plugins: [
                 require('@tailwindcss/typography'),
